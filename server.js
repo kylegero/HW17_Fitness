5f61835c071aa74fe115100ad8a95146510a7c20
Kyle Pykonen <kylepyk@gmail.com>
2020-12-30 23:16:31 -0800

    added new folder with two new files

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 8080;
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, { 
   useNewUrlParser: true,
   useFindAndModify: false
-});
+})
 
 app.use(require("./routes/api.js"));
+app.use("/", require("./routes/html.js"));
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
